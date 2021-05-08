209d67f449a7d5ed4f11d380786db069241e280f
valentinaQuiroga <valequiroga87@gmail.com>
2021-05-09 00:56:47 +0100

    Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import depots.Depot;
+import factory.DepotFactory;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Depot d1 = DepotFactory.getDepot("a");
+        Depot d2 = DepotFactory.getDepot("b");
+        Depot d3 = DepotFactory.getDepot("c");
+
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+
+
+
+
     }
-}
+
+  }
